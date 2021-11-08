a2a606ef75fa2d32b6b3588281c2ca482eb76a7e
magnetenstad <tenstad.magne@gmail.com>
2021-11-08 17:49:15 +0100

    Edit merge_sort

--- lib/merge_sort.py
+++ lib/merge_sort.py
@@ -1,41 +1,9 @@
-__author__ = "Henrik Høiness"
+from merge import merge
+from math import floor
 
-
-def merge_sort(A):
-    if len(A)>1:
-        q = len(A)//2
-        lh = merge_sort(A[:q])
-        rh = merge_sort(A[q:])
-        return merge(lh,rh)
-
-    return A
-
-
-
-def merge(lh,rh):
-    res = []
-    i = 0
-    j = 0
-
-    while i<len(lh) and j<len(rh):
-        if lh[i] < rh[j]:
-            res.append(lh[i])
-            i+=1
-
-        else:
-            res.append(rh[j])
-            j+=1
-
-    if i<len(lh): res.extend(lh[i:])
-    if j<len(rh): res.extend(rh[j:])
-    return res
-
-
-
-
-def main():
-    unsorted_list = [3,5,2,6,99,8,4]
-    sorted_list = merge_sort(unsorted_list)
-    print(sorted_list)
-
-main()
+def merge_sort(A, p, r):
+    if p < r:
+        q = floor((p + r) / 2)
+        merge_sort(A, p, q)
+        merge_sort(A, q + 1, r)
+        return merge(A, p, q, r)
