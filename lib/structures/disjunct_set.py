e8ea97d74304d71345dfc62904d4a4768562298a
magnetenstad <tenstad.magne@gmail.com>
2021-11-16 16:47:56 +0100

    Implement disjoint_set, fix bubblesort typo

--- lib/structures/disjunct_set.py
+++ lib/structures/disjunct_set.py
@@ -1,19 +1,36 @@
 
-def connected_components():
-    pass
 
-def same_component():
-    pass
+class Graph:
+    V = set()
+    E = set()
 
-def make_set():
-    pass
+def connected_components(G):
+    for v in G.V:
+        make_set(v)
+    for u, v in G.E:
+        if find_set(u) != find_set(v):
+            union(u, v)
 
-def union():
-    pass
+def same_component(u, v):
+    return find_set(u) == find_set(v)
+    
+def make_set(x):
+    x.p = x
+    x.rank = 0
 
-def link():
-    pass
+def union(x, y):
+    link(find_set(x), find_set(y))
 
-def find_set():
-    pass
+def link(x, y):
+    if x.rank > y.rank:
+        y.p = x
+    else:
+        x.p = y
+        if x.rank == y.rank:
+            y.rank += 1
+
+def find_set(x):
+    if x != x.p:
+        x.p = find_set(x.p)
+    return x.p
 
