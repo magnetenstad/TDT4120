599bf8b882b42cdc5ff48adc988a05a9010a4777
magnetenstad <tenstad.magne@gmail.com>
2021-11-16 17:05:08 +0100

    Implement all_pairs_shortest_paths

--- lib/all_pairs_shortest_path.py
+++
@@ -1,9 +0,0 @@
-
-def print_all_pairs_shortest_path():
-    pass
-
-def slow_all_pairs_shortest_paths():
-    pass
-
-def faster_all_pairs_shortest_paths():
-    pass
---
+++ lib/all_pairs_shortest_paths.py
@@ -0,0 +1,39 @@
+
+def extend_shortest_paths(L, W):
+    n = len(L)
+    l = [[None] * n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            l[i][j] = float('inf')
+            for k in range(n):
+                l[i][j] = min(l[i][j], l[i][k] + W[k][j])
+    return l
+
+def print_all_pairs_shortest_path(Pi, i, j):
+    if i == j:
+        print(i)
+    elif Pi[i][j] == None:
+        print(f"No path from {i} to {j} exists.")
+    else:
+        print_all_pairs_shortest_path(Pi, i, Pi[i][j])
+        print(j)
+
+def slow_all_pairs_shortest_paths(W):
+    n = len(W)
+    L = [None] * (n)
+    L[0] = W
+    for m in range(1, n):
+        L[m] = [[None] * n for _ in range(n)]
+        L[m] = extend_shortest_paths(L[m - 1], W)
+    return L[n - 1]
+
+def faster_all_pairs_shortest_paths(W):
+    n = len(W)
+    L = [None] * (n)
+    L[0] = W
+    m = 1
+    while m < n - 1:
+        L[2 * m] = [[None] * n for _ in range(n)]
+        L[2 * m] = extend_shortest_paths(L[m], L[m])
+        m *= 2
+    return L[m - 1]
