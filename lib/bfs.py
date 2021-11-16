aeb40d2bebe2bea3230d22c0de835f4af6692fc1
magnetenstad <tenstad.magne@gmail.com>
2021-11-16 17:26:45 +0100

    Edit queue, implement bfs

---
+++ lib/bfs.py
@@ -0,0 +1,22 @@
+from structures.queue import Queue, enqueue, dequeue
+
+def BFS(G, s):
+    for u in G.V - set(s):
+        u.color = "white"
+        u.d = float('inf')
+        u.pi = None
+    s.color = "gray"
+    s.d = 0
+    s.pi = None
+    Q = Queue()
+    enqueue(Q, s)
+    while Q:
+        u = dequeue(Q)
+        for v in G.Adj[u]:
+            if v.color == "white":
+                v.color = "gray"
+                v.d = u.d + 1
+                v.pi = u
+                enqueue(Q, v)
+        u.color = "black"
+    
--- lib/structures/queue.py
+++ lib/structures/queue.py
@@ -1,27 +1,45 @@
 
-class Queue(dict):
-    def __init__(self, length) -> None:
-        self.length = length
-        self.tail = 0
-        self.head = 0
+# 1) Implementation from Cormen
+
+# class Queue(list):
+#     def __init__(self, length) -> None:
+#         self.length = length
+#         self.extend([None] * length)
+#         self.tail = 0
+#         self.head = 0
+
+# def enqueue(Q, x):
+#     Q[Q.tail] = x
+#     if Q.tail == Q.length:
+#         Q.tail = 0
+#     else:
+#         Q.tail += 1
+
+# def dequeue(Q):
+#     x = Q[Q.head]
+#     if Q.head == Q.length:
+#         Q.head = 0
+#     else:
+#         Q.head += 1
+#     return x
+
+
+# 2) Better implementation in Python
+
+from collections import deque
+
+class Queue(deque):
+    pass
 
 def enqueue(Q, x):
-    Q[Q.tail] = x
-    if Q.tail == Q.length:
-        Q.tail = 0
-    else:
-        Q.tail += 1
+    Q.appendleft(x)
 
 def dequeue(Q):
-    x = Q[Q.head]
-    if Q.head == Q.length:
-        Q.head = 0
-    else:
-        Q.head += 1
-    return x
+    return Q.pop()
+
 
 if __name__ == '__main__':
-    Q = Queue(5)
+    Q = Queue()
     enqueue(Q, 1)
     enqueue(Q, 5)
     enqueue(Q, 7)
